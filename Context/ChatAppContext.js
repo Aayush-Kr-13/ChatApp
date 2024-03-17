f3d5ecae2638fe9f4906386e0dbb1901fbecfd5a
Ashish Lal <ashishlal2003@gmail.com>
2024-03-17 10:03:03 +0530

    working _app.js

--- Context/ChatAppContext.js
+++ Context/ChatAppContext.js
@@ -3,14 +3,13 @@ import {useRouter} from "next/router";
 
 import {ChechIfWalletConnected, connectWallet, connectingWithContract} from "@/Utils/apiFeature";
 
-export const ChatAppContext = React.createContext();
-
 export const ChatAppProvider = ({children}) => {
     const title = "Hey Welcome to ChatApp by Aayush and Ashish";
 
     return(
-        <ChatAppContext.Provider value={{ title }}>
+        <ChatAppContext.Provider value={{title}}>
             {children}
         </ChatAppContext.Provider>
     );
 };
+export const ChatAppContext = React.createContext(ChatAppProvider);
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import React, {useEffect, useState, useContext} from "react"
 import { ChatAppContext } from "@/Context/ChatAppContext"
 
 const ChatApp = () => {
-    const { title } = useContext(ChatAppContext);
+    const  {title}  = useContext(ChatAppContext);
+    console.log(title);
     return <div>{title}</div>;
 }
 
