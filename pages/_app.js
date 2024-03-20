51b4d9d74dc5fe80f2fdd0aaf005a88de4a98a43
Ashish Lal <ashishlal2003@gmail.com>
2024-03-20 19:16:31 +0530

    error jsx added

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,13 @@ import "../styles/global.css"
 
 import { ChatAppProvider } from "@/Context/ChatAppContext";
 import { NavBar } from "@/Components";
-
+import { Error } from "@/Components";
 const MyApp = ({Component, pageProps }) => (
     <div>
         <ChatAppProvider>
             <NavBar/>
             <Component {...pageProps} />
+            {/* <Error /> */}
         </ChatAppProvider>
     </div>
 );
